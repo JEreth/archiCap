b052229febfb153c729a2f7e9b2fdfad70556bc4
Julian <j.ereth@trendmarke.de>
2019-04-27 20:01:18 +0200

    import export current profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {Capability} from '../../capabilities/shared/capability';
 import {CapabilityService} from '../../capabilities/shared/capability.service';
 import {SystemService} from '../../systems/shared/system.service';
-import {ProfileService} from '../../shared/profile.service';
+import {ProfilePersistence, ProfileService} from '../../shared/profile.service';
 import {System} from '../../systems/shared/system';
 import {MatSnackBar} from '@angular/material';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-profile',
@@ -14,13 +15,18 @@ import {MatSnackBar} from '@angular/material';
 export class ProfileComponent implements OnInit {
 
   public selectedSystems: number[];
-  public selectedCapabilities: number[]
+  public selectedCapabilities: number[];
   public capabilities: Capability[] = [];
   public systems: System[] = [];
+
+  public downloadJsonHref: SafeUrl;
+  public uploadFile: any;
+  public uploading = false;
 
   constructor(private capabilityService: CapabilityService,
               private systemService: SystemService,
               private snackBar: MatSnackBar,
+              private sanitizer: DomSanitizer,
               private profile: ProfileService) {
 
     this.capabilityService.getAllAsArray().subscribe( capabilities => {
@@ -34,6 +40,7 @@ export class ProfileComponent implements OnInit {
     this.profile.init().subscribe( () => {
       this.selectedSystems = this.profile.selectedSystems;
       this.selectedCapabilities = this.profile.selectedCapabilities;
+      this.generateDownload();
     });
   }
 
@@ -45,7 +52,44 @@ export class ProfileComponent implements OnInit {
     this.profile.selectedCapabilities = this.selectedCapabilities;
     this.profile.persist().subscribe( () => {
       this.snackBar.open('Profile was saved');
+      this.generateDownload();
     });
+  }
+
+  generateDownload() {
+    const url = 'data:text/json;charset=UTF-8,' + encodeURIComponent(this.profile.exportToJson());
+    this.downloadJsonHref = this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  uploadFileChanged(e) {
+    this.uploadFile = e.target.files[0];
+  }
+
+  upload() {
+    this.uploading = true;
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      try {
+        const input = JSON.parse(fileReader.result.toString());
+        if (this.profile.validate(input)) {
+          // schema looks good so go on and delete old config if it exists
+          this.profile.importFromPersistence(<ProfilePersistence> input).subscribe( () => {
+            this.snackBar.open('Profile has been imported.');
+            this.uploading = false;
+            this.selectedSystems = this.profile.selectedSystems;
+            this.selectedCapabilities = this.profile.selectedCapabilities;
+          });
+        } else {
+          // error
+          this.snackBar.open('The uploaded profile is not valid.');
+          this.uploading = false;
+        }
+      } catch (e) {
+        this.snackBar.open('There has been an error. Please try again.');
+        this.uploading = false;
+      }
+    };
+    fileReader.readAsText(this.uploadFile);
   }
 
 }
--- src/app/shared/profile.service.ts
+++ src/app/shared/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import { Observable, of} from 'rxjs';
 import {DataService} from './data.service';
 
 export interface ProfilePersistence {
@@ -38,6 +38,33 @@ export class ProfileService {
       selectedSystems: this.selectedSystems
     };
     return this.data.set('profile', p);
+  }
+
+  exportToJson(): string {
+    const p: ProfilePersistence = {
+      selectedCapabilities: this.selectedCapabilities,
+      selectedSystems: this.selectedSystems
+    };
+    return JSON.stringify(p);
+  }
+
+  validate(input: any): boolean {
+    // we only check for the major properties here. Might be better to make a in-depth validation
+    return (input &&
+      input.selectedCapabilities && typeof(input.selectedCapabilities) === 'object' &&
+      input.selectedSystems && typeof(input.selectedSystems) === 'object');
+  }
+
+  reset(): Observable<boolean> {
+    this.selectedCapabilities = [];
+    this.selectedSystems = [];
+    return this.persist();
+  }
+
+  importFromPersistence(input: ProfilePersistence): Observable<boolean> {
+    this.selectedCapabilities = input.selectedCapabilities || [];
+    this.selectedSystems = input.selectedSystems || [];
+    return this.persist();
   }
 
 }
