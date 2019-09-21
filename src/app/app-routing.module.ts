f39755b336408fa991f28033c43cc26fc635e18e
Julian <j.ereth@pragmatic-apps.de>
2019-09-21 11:13:24 +0200

    angular 8 upgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'components', loadChildren: './systems/systems.module#SystemsModule' },
-  { path: 'patterns', loadChildren: './patterns/patterns.module#PatternsModule' },
-  { path: 'products', loadChildren: './products/products.module#ProductsModule' },
-  { path: 'categories', loadChildren: './categories/categories.module#CategoriesModule' },
-  { path: 'capabilities', loadChildren: './capabilities/capabilities.module#CapabilitiesModule' },
-  { path: 'configuration', loadChildren: './pages/configuration/configuration.module#ConfigurationModule' },
-  { path: 'about', loadChildren: './pages/about/about.module#AboutModule' },
-  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfileModule' },
-  { path: 'stage', loadChildren: './pages/stage/stage.module#StageModule' },
-  { path: 'explore', loadChildren: './pages/explore/explore.module#ExploreModule' },
-  { path: 'wizard', loadChildren: './pages/wizard/wizard.module#WizardModule' },
+  { path: 'components', loadChildren: () => import('./systems/systems.module').then(m => m.SystemsModule) },
+  { path: 'patterns', loadChildren: () => import('./patterns/patterns.module').then(m => m.PatternsModule) },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: 'categories', loadChildren: () => import('./categories/categories.module').then(m => m.CategoriesModule) },
+  { path: 'capabilities', loadChildren: () => import('./capabilities/capabilities.module').then(m => m.CapabilitiesModule) },
+  { path: 'configuration', loadChildren: () => import('./pages/configuration/configuration.module').then(m => m.ConfigurationModule) },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'stage', loadChildren: () => import('./pages/stage/stage.module').then(m => m.StageModule) },
+  { path: 'explore', loadChildren: () => import('./pages/explore/explore.module').then(m => m.ExploreModule) },
+  { path: 'wizard', loadChildren: () => import('./pages/wizard/wizard.module').then(m => m.WizardModule) },
   { path: '', redirectTo: '/about', pathMatch: 'full'},
 ];
 
