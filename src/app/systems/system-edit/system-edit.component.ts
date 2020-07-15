b4f44b0f86ad1cd8b9391893da13acda9397757a
Julian <j.ereth@pragmatic-apps.de>
2020-07-15 16:59:56 +0200

    add relation checker in systems

--- src/app/systems/system-edit/system-edit.component.ts
+++ src/app/systems/system-edit/system-edit.component.ts
@@ -4,6 +4,7 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 import {System} from '../shared/system';
 import {SystemService} from '../shared/system.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Configuration, ConfigurationService} from '../../shared/configuration.service';
 
 @Component({
   selector: 'app-system-edit',
@@ -14,23 +15,29 @@ export class SystemEditComponent implements OnInit {
 
   public form: FormGroup;
   public system: System = {name: '', description: '', categories: [], products: [], substitutions: []};
+  public configuration: Configuration;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private systemService: SystemService,
     private snackBar: MatSnackBar,
-    private formBuilder: FormBuilder
-  ) {  }
+    private formBuilder: FormBuilder,
+    private configurationService: ConfigurationService
+  ) {}
 
   async ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.system = (await this.systemService.get(id)) as System || this.system;
     }
+    this.configuration = await this.configurationService.get();
     this.form = this.formBuilder.group({
       name: [this.system.name, Validators.required],
-      description: [this.system.description]
+      description: [this.system.description],
+      categories: [this.system.categories],
+      products: [this.system.products],
+      substitutions: [this.system.substitutions],
     });
   }
 
